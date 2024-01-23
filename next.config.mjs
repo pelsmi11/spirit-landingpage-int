774aea869cf9639c46ac06fe7b4dbed81d6b9410
pelsmi11 <53068447+pelsmi11@users.noreply.github.com>
2024-01-22 22:23:51 -0600

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+export default withNextIntl();
